6100c15c947ca5e27b6fb6f1c1bdd88ab74c7b08
wfer0011 <116248576+wfer0011@users.noreply.github.com>
2022-10-31 23:23:20 +1100

    Assignment 2 files added

---
+++ maps.js
@@ -0,0 +1,40 @@
+var vg_1 = "GDP_PCWASTE.json";
+vegaEmbed("#globe", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "missmanaged_new_map.vg.json";
+vegaEmbed("#misswaste", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "mismanaged_ocean_vg.json";
+vegaEmbed("#oceanmisswaste", vg_3).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "world_plastic.vg.json";
+vegaEmbed("#plasticyears", vg_4).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+var vg_5 = "china_export.json";
+vegaEmbed("#chinaexport", vg_5).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "product_sectors.vg.json";
+vegaEmbed("#productvg", vg_6).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_7 = "disposal_chart.vg.json";
+vegaEmbed("#areachart", vg_7).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_8= "gdp_percapita_ocean.vg.json";
+vegaEmbed("#scatterplot", vg_8).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
